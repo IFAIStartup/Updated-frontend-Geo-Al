81512df61820ae00585f4d30cb1f52e5c2c3f734
IFAIStartup <ifaistartup@gmail.com>
2024-10-30 23:54:40 +0530

    Add files via upload
    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'Anton' : ["Anton SC", "sans-serif"],
+      'Krona' : ["Krona One", "serif"],
+      'Chela' : ["Chela One", "system-ui"],
+      'RocknRoll' : ["RocknRoll One", "sans-serif"],
+      'Josefin ' : ["Josefin Sans", "serif"],
+      "Poppins" : ["Poppins", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
